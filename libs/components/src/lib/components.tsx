14d79b85aa1ad8963eea55d0e29cef5e1611aefb
Cenk Kılıç <cenk@kilic.dev>
2020-10-30 19:00:17 +0100

    fix: fixed basic setup

---
+++ libs/components/src/lib/components.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+/* eslint-disable-next-line */
+export interface ComponentsProps {}
+
+const StyledComponents = styled.div`
+  color: pink;
+`
+
+export const Components: React.FunctionComponent<ComponentsProps> = () => {
+  return (
+    <StyledComponents>
+      <h1>Welcome to components!</h1>
+    </StyledComponents>
+  )
+}
+
+export default Components
