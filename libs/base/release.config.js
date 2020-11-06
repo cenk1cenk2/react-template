360f40d271f81514f36dd42df2f2252660c2268c
Cenk Kılıç <cenk@kilic.dev>
2020-11-06 23:35:14 +0100

    ci: added drone setup and semantic release

---
+++ libs/base/release.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: [ 'CHANGELOG.md', 'package.json', 'README.md' ]
+      }
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'cd ../../ && yarn build:one base'
+      }
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        pkgRoot: '../../dist/react-template-base'
+      }
+    ]
+  ]
+}
